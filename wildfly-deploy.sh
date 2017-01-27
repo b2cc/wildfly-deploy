4624fd8d76f5ddf90234dd651319ff53dd68429a
b2cc <b2c@dest-unreachable.net>
2017-01-27 14:58:34 +0100

    Add files via upload
    script cleanup/lint
    couple more checks
    TODO added

--- wildfly-deploy.sh
+++ wildfly-deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash 
 
 # universal deploy script for jenkins->wildfly deployments
-# dg@upstream
+# b2c@dest-unreachable.net
 # 24.01.2017
 # License: GPLv2
 
-# variables
 SSHUSER=""
 SSHPASSWD=""
 SERVER=""
@@ -13,60 +12,45 @@ CONTROLLER_CLI=""
 CONTROLLER_HOST=""
 CONTROLLER_PORT=""
 HAPROXY_MGMT="0"
-
-
-if ! [[ -x $(which sshpass) ]]; then
-  echo "sshpass binary not found, exiting"
-else
-  SSHPASS="$(which sshpass)"
-fi
-
-if ! [[ -x $(which ssh) ]]; then
-  echo "ssh binary not found, exiting"
-else
-  SSH="$(which ssh)"
-fi
-
-if ! [[ -x $(which scp) ]]; then
-  echo "scp binary not found, exiting"
-else
-  SCP="$(which scp)"
-fi
+HAPROXY_HOST=""
+HAPROXY_PORT=""
+HAPROXY_BACKEND=""
 
 help(){
 echo "
  wildfly-deploy.sh
   a post-build deploy script to deploy war files to jboss/wildfly
 
+  requires: sshpass, scp, ssh
+  optional: socat (for haproxy management during deployment)
+
   options:
   -C   path to jboss or wildfly cli (on remote host,
          e.g.: /opt/wildfly/bin/jboss-cli.sh)
-  -H   host of the wildfly console (e.g.: localhost)
-  -P   port of the wildfly console (e.g.: 9990)
-  -S   server to deploy to. can be specified multiple times,
+  -H   host of the wildfly console (on remote host, e.g.: localhost)
+  -P   port of the wildfly console (on remote host, e.g.: 9990)
+  -S   remote server(s) to deploy to. can be specified multiple times,
          e.g.: -S server1 -S server2 ...
   -h   display help
-  -u   ssh user to connect to server(s)
-  -p   ssh password to connect to server(s)
+  -u   ssh user to connect to remote server(s)
+  -p   ssh password to connect to remote server(s)
 
   ## HAProxy Options ##
 
-   if enabled with the '-e' switch, haproxy backend servers can
-   be automatically disabled / re-enabled during deployment.
-   the backend to operate on must be specified, the servernames
-   will be taken from the '-S' parameters and must match the
-   backend server names.
+  if enabled with the '-e' switch, haproxy backend servers can
+  be automatically disabled / re-enabled during deployment.
+  the backend to operate on must be specified, the servernames
+  will be taken from the '-S' parameter which must match the
+  server names configured in the haproxy backend.
 
-  -e   enable HAProxy management during Deployment
-  -A   haproxy server name
+  -e   enable HAProxy management during deployment
+  -A   haproxy server name / IPv4 address
   -a   haproxy control socket port (stats socket: level admin)
-  -b   HTTP Backend
+  -b   HTTP backend
 "
 exit 0
 }
 
-
-# test arguments
 if (($# == 0)); then
   echo "
          missing arguments, exiting.
@@ -113,6 +97,17 @@ while getopts "C:H:P:S:hu:p:eA:a:b:" opt; do
 done
 shift $((OPTIND -1))
 
+BINARIES=(sshpass ssh scp)
+for binary in "${BINARIES[@]}" ; do
+  if ! [[ -x $(which ${binary} 2>/dev/null) ]]; then
+    echo "${binary} binary not found, exiting."
+    exit 1
+  fi
+done
+
+SSHPASS="$(which sshpass)"
+SSH="$(which ssh)"
+SCP="$(which scp)"
 
 echo "## ** ENVIRONMENT ** ##
 
@@ -126,31 +121,36 @@ echo " ## ** DEPLOYMENT SUMMARY ** ##
  host:        ${CONTROLLER_HOST}
  port:        ${CONTROLLER_PORT}
  sshuser:     ${SSHUSER}
- sshpassword: ${SSHPASSWD}
+ sshpassword: ********
 
  local artifact:  ${POM_ARTIFACTID}-${POM_VERSION}
 
- ssh:     ${SSH}
- scp:     ${SCP}
- sshpass: ${SSHPASS}
-
  deploy to:"
-for server in ${SERVER[@]}; do
+for server in "${SERVER[@]}"; do
   echo "        - ${server}"
 done  
 
 if [[ "${HAPROXY_MGMT}" -ne 0 ]]; then
-  TALK2HAPROXY="socat stdio tcp4-connect:${HAPROXY_HOST}:${HAPROXY_PORT}"
+  if ! [[ -x $(which socat 2>/dev/null) ]]; then
+    echo "socat binary not found, exiting."
+    exit 1
+  else
+    SOCAT="$(which socat)"
+  fi
+
+  TALK2HAPROXY="${SOCAT} stdio tcp4-connect:${HAPROXY_HOST}:${HAPROXY_PORT}"
   echo "
  ## HAProxy Management during Deployment enabled ##
- HAProxy Host:    "${HAPROXY_HOST}"
- HAProxy Port:    "${HAPROXY_PORT}"
- HAProxy Backend: "${HAPROXY_BACKEND}"
+ HAProxy Host:    ${HAPROXY_HOST}
+ HAProxy Port:    ${HAPROXY_PORT}
+ HAProxy Backend: ${HAPROXY_BACKEND}
 
- HAProxy Backend "${HAPROXY_BACKEND}" servers:
+ HAProxy Backend ${HAPROXY_BACKEND} servers:
 "
-  echo "show servers state ${HAPROXY_BACKEND}" | ${TALK2HAPROXY} | grep "${HAPROXY_BACKEND}" | awk '{print "  backend: "$2,"  - server: "$4, "/ ip: "$5 }'
-
+  echo "show servers state ${HAPROXY_BACKEND}" \
+    | ${TALK2HAPROXY} \
+    | grep "${HAPROXY_BACKEND}" \
+    | awk '{print "  backend: "$2,"  - server: "$4, "/ ip: "$5 }'
 fi
 
 echo "
@@ -159,30 +159,32 @@ echo "
  ###############################
 "
 
-for server in ${SERVER[@]}; do
+for server in "${SERVER[@]}"; do
 
   # determine remote artifact
   REMOTE_ARTIFACT=$( ${SSHPASS} -p ${SSHPASSWD} \
                        ${SSH} -o StrictHostKeyChecking=no \
                        ${SSHUSER}@${server} "${CONTROLLER_CLI} \
-                          --connect \
-                          --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
-                          --commands='ls deployment' \
-                          | grep ${POM_ARTIFACTID}"
+                       --connect \
+                       --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
+                       --commands='ls deployment' \
+                       | grep ${POM_ARTIFACTID}"
                    )
   echo -en "\n
  - remote server ${server} currently deployed artifact: ${REMOTE_ARTIFACT}
    starting deployment:\n"
 
   # copy new artifact to server(s)
-  echo "  -- copy new artifact ${POM_ARTIFACTID}-${POM_VERSION} to server ${server}..."
+  echo "  -- copy new artifact ${POM_ARTIFACTID}-${POM_VERSION} to server ${server} /tmp..."
   ${SSHPASS} -p ${SSHPASSWD} \
-    ${SCP} -o StrictHostKeyChecking=no "${WORKSPACE}/target/${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING}" \
-    ${SSHUSER}@${server}:/tmp/
-  if ! [[ $? -eq 0 ]]; then echo "copy failed, exiting"; exit 1; fi
+                  ${SCP} -o StrictHostKeyChecking=no "${WORKSPACE}/target/${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING}" \
+                  ${SSHUSER}@${server}:/tmp/
+  if ! [[ $? -eq 0 ]]; then echo "copy failed, exiting."; exit 1; fi
   echo "  -- copy finished."
 
   # set haproxy backend server to MAINT
+  # TODO: wait until all clients have migrated to other server
+  #       in case of multi-server deployment
 
   if [[ "${HAPROXY_MGMT}" -ne 0 ]]; then
     echo "  -- setting server ${server} on HAProxy backend ${HAPROXY_BACKEND} to MAINT"
@@ -194,24 +196,30 @@ for server in ${SERVER[@]}; do
   ${SSHPASS} -p ${SSHPASSWD} \
                   ${SSH} -o StrictHostKeyChecking=no \
                   ${SSHUSER}@${server} "${CONTROLLER_CLI} \
-                    --connect \
-                    --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
-                    --commands='undeploy ${REMOTE_ARTIFACT}'"
-  if ! [[ $? -eq 0 ]]; then echo "undeploy failed, exiting"; exit 1; fi
+                  --connect \
+                  --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
+                  --commands='undeploy ${REMOTE_ARTIFACT}'"
+  if ! [[ $? -eq 0 ]]; then echo "undeploy failed, exiting."; exit 1; fi
   echo "  -- undeployment of old artifact finished."
 
   # deploy new artifact
   echo "  -- start deployment of new artifact ${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING} on ${server}"
-   ${SSHPASS} -p ${SSHPASSWD} \
+  ${SSHPASS} -p ${SSHPASSWD} \
                   ${SSH} -o StrictHostKeyChecking=no \
                   ${SSHUSER}@${server} "${CONTROLLER_CLI} \
-                    --connect \
-                    --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
-                    --commands='deploy /tmp/${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING}'"
-  if ! [[ $? -eq 0 ]]; then echo "deploy failed, exiting"; exit 1; fi
+                  --connect \
+                  --controller=${CONTROLLER_HOST}:${CONTROLLER_PORT} \
+                  --commands='deploy /tmp/${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING}'"
+  if ! [[ $? -eq 0 ]]; then echo "deploy failed, exiting."; exit 1; fi
   echo -en "  -- deployment of new artifact ${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING} on ${server} finished.\n\n"
 
-  # set haproxy backend server to MAINT
+  # cleanup
+  echo "  -- removing temporary copy of ${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING} on ${server} from /tmp"
+  ${SSHPASS} -p ${SSHPASSWD} \
+                  ${SSH} -o StrictHostKeyChecking=no \
+                  ${SSHUSER}@${server} "rm -f /tmp/${POM_ARTIFACTID}-${POM_VERSION}.${POM_PACKAGING}"
+
+  # set haproxy backend server to READY
   if [[ "${HAPROXY_MGMT}" -ne 0 ]]; then
     echo "  -- setting server ${server} on HAProxy backend ${HAPROXY_BACKEND} to READY"
     echo "set server ${HAPROXY_BACKEND}/${server} state ready" | ${TALK2HAPROXY}
